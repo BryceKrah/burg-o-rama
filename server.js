d420b199ce04709ef09f0046bfda544195084327
Bryce Krah <krahbryce@yahoo.com>
2016-02-22 18:50:41 -0500

    finished ex2

--- server.js
+++ server.js
@@ -3,44 +3,24 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-
+var burgerRoutes = require(path.join(__dirname, '/routes/burger'));
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
-
-
-//  `/`: to show a homepage.
-//  `/burgers`: to create a burger or list burgers (depending on the HTTP verb used [see [wiki link](https://en.wikipedia.org/wiki/Representational_state_transfer)]).
-//  `/burgers/:id`: to view/edit/delete ONE burger.
-//  `/burgers/new`: to view the 'new burger' form.
-//  `/burgers/:id/edit`: to view the 'edit burger' form.
 
 app.get('/', function(req,res){
   res.send("heres the homepage")
 });
 
-app.get('/burgers', function(req,res){
-  res.send("this is the burger GET route")
-})
-   .post('/burgers', function(req,res){
-     res.send("this is the burger POST route")
-   });
-
-app.get('/burgers/:id', function(req,res){
-  res.send("this is the edit or PUT route")
-});
-
-app.get('/burgers/new', function(req,res){
-  res.send("this is the view the new burger form")
-});
-
-app.get('/burgers/:id/edit', function(req,res){
-  res.send("this is the view the edit burger form")
-});
+app.use('/burgers', burgerRoutes);
 
 
 
